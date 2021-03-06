9c5d5d11a4ea6d44f5f04e2a887f9c35abe196ea
aspyring <abhaychemist@gmail.com>
2021-03-06 13:29:01 +0400

    Update suntzu.py

--- cogs/suntzu.py
+++ cogs/suntzu.py
@@ -20,7 +20,7 @@ with open('quotes.json', 'r') as f:
 
 routes = web.RouteTableDef()
 
-font = ImageFont.truetype('suntzu.ttf', 48)
+font = ImageFont.truetype('georgia.ttf', 48)
 
 def wrap_text(text, line_length):
  output = ''
