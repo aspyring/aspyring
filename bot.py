1922a764fc35b6d378a18210cd432ccd640a52b7
aspyring <abhaychemist@gmail.com>
2021-03-06 12:24:08 +0400

    Removed cog.rules

--- bot.py
+++ bot.py
@@ -27,7 +27,6 @@ client.load_extension('cogs.mod')
 client.load_extension('cogs.user')
 client.load_extension('cogs.Bot')
 client.load_extension('cogs.modmail')
-client.load_extension('cogs.rules')
 client.load_extension('cogs.error_handling')
 
 #===================================== EVENTS =======================================#
